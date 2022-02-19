de615af2f3356596ddf3914c00cadd4938b7d92a
Juan Jiménez <nefioxdev@gmail.com>
2022-02-19 00:02:36 -0600

    Estructura inicial

---
+++ api/components/user/network.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const response = require("../../../network/response");
+
+router.get("/", (req, res) => {
+  //   res.send("Todo funciona");
+  response.success(req, res, "Todo correcto", 200);
+});
+
+module.exports = router;
