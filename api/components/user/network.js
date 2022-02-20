d40bb037a34c44051b958633bc20ce70581b8aa5
Juan Jiménez <nefioxdev@gmail.com>
2022-02-20 00:02:18 -0600

    Documentando la API

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -3,25 +3,29 @@ const router = express.Router();
 const Controller = require("./index");
 const response = require("../../../network/response");
 
-router.get("/", (req, res) => {
+router.get("/", list);
+router.get("/:id", get);
+router.post("/", upsert);
+
+// Internal functions
+function list(req, res) {
   Controller.list()
     .then((lista) => {
       response.success(req, res, lista, 200);
     })
     .catch((err) => response.error(req, res, err.message, 500));
-});
+}
 
-router.get("/:id", (req, res) => {
+function get(req, res) {
   Controller.get(req.params.id)
     .then((user) => response.success(req, res, user, 200))
     .catch((err) => response.error(req, res, err.message, 500));
-});
+}
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+function upsert(req, res) {
   Controller.upsert(req.body)
     .then((data) => response.success(req, res, data, 200))
     .catch((err) => response.error(req, res, err.message, 500));
-});
+}
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const config = require("../config.js");
 const user = require("./components/user/network");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 // Routing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const swaggerDoc = require("./swagger.json");
+
 app.use("/api/user", user);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // Server
 app.listen(config.api.port, () =>
