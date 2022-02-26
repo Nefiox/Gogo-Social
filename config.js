87c8fd6caac2491b345e44667dc112e8b346327a
Juan Jiménez <nefioxdev@gmail.com>
2022-02-25 22:47:25 -0600

    Vercel

--- config.js
+++ config.js
@@ -1,22 +1,25 @@
+require("dotenv").config();
+
 module.exports = {
+  remoteDB: process.env.REMOTE_DB || false,
   api: {
-    port: process.env.API_PORT || 3000,
+    port: process.env.API_PORT,
   },
   jwt: {
-    secret: process.env.JWT_SECRET || "notasecret",
+    secret: process.env.JWT_SECRET,
   },
   mysql: {
-    host: process.env.MYSQL_HOST || "remotemysql.com",
-    user: process.env.MYSQL_USER || "Ck2viEAoqf",
-    password: process.env.MYSQL_PASS || "rSiNBrVnoJ",
-    database: process.env.MYSQL_DB || "Ck2viEAoqf",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_DB,
   },
   mysqlService: {
-    host: process.env.MYSQL_SRV_HOST || "localhost",
-    port: process.env.MYSQL_SRV_PORT || 3001,
+    host: process.env.MYSQL_SRV_HOST,
+    port: process.env.MYSQL_SRV_PORT,
   },
   post: {
-    host: process.env.POST_HOST || "localhost",
-    port: process.env.POST_PORT || 3002,
+    host: process.env.POST_HOST,
+    port: process.env.POST_PORT,
   },
 };
