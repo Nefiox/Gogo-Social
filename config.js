d2240d6f61d1c5e706c79cd2679f81f3259270f8
Juan Jiménez <nefioxdev@gmail.com>
2022-02-26 12:36:25 -0600

    Conectando Redis

--- config.js
+++ config.js
@@ -18,8 +18,19 @@ module.exports = {
     host: process.env.MYSQL_SRV_HOST,
     port: process.env.MYSQL_SRV_PORT,
   },
+  cacheService: {
+    host: process.env.REDIS_SRV_HOST || "localhost",
+    port: process.env.REDIS_SRV_PORT || 3003,
+  },
   post: {
     host: process.env.POST_HOST,
     port: process.env.POST_PORT,
+  },
+  redis: {
+    host:
+      process.env.REDIS_HOST ||
+      "redis-15101.c244.us-east-1-2.ec2.cloud.redislabs.com",
+    port: process.env.REDIS_PORT || 15101,
+    password: process.env.REDIS_PASS || "qvPrbK9EUC9zvPQ2gIN5qEvB90dSEnji",
   },
 };
